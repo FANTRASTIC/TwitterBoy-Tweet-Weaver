53553e1b02376ed6a642a1dbe4c30dd97d561f76
Saugat Pariyar <saugatpariyar80@gmail.com>
2025-11-02 20:01:14 +0000

    You’re hitting a **Next/Image remote host** restriction, not a key/confi

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      // NewsAPI common domains
+      { protocol: 'https', hostname: 'media.wired.com' },
+      { protocol: 'https', hostname: 'static01.nyt.com' },
+      { protocol: 'https', hostname: 'i.guim.co.uk' },
+      { protocol: 'https', hostname: 'cdn.cnn.com' },
+      { protocol: 'https', hostname: 'ichef.bbci.co.uk' },
+      { protocol: 'https', hostname: 'assets.bwbx.io' },
+      { protocol: 'https', hostname: 'images.wsj.net' },
+      { protocol: 'https', hostname: 's.yimg.com' },
+      { protocol: 'https', hostname: 'techcrunch.com' },
+      { protocol: 'https', hostname: '*.google.com' },
+
     ],
   },
 };
